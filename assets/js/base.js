865ab4654eabd84e5ca96c0a5ad9a640c04c9cd7
Andrej Stevkovski <stevandrej@yahoo.com>
2021-04-27 19:55:16 +0200

    LocalStorage for theme selection

---
+++ assets/js/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+	cardsContainer: document.getElementById('hero-cards'),
+	overviewCardsContainer: document.getElementById('overview-cards'),
+	themeCheckbox: document.getElementById('theme-toggle')
+}
--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,11 +1,35 @@
+import { elements } from './base.js';
 import { animateCounter, abbreviateNumber } from './utils.js';
 
-//Switch theme style
-document.getElementById('theme-toggle').addEventListener('change', () => {
-	document.body.className === 'light' ? document.body.className = 'dark' : document.body.className = 'light';
-})
+//THEME CONTROLLER
+const themeController = () => {
+	//Theme Button functionality
+	elements.themeCheckbox.addEventListener('change', () => {
+		if (document.body.className === 'light') {
+			document.body.className = 'dark'
+			localStorage.setItem('theme', 'dark');
+		}
+		else {
+			document.body.className = 'light';
+			localStorage.setItem('theme', 'light')
+		}
+	});
+
+	//Theme localStorage
+	const theme = localStorage.getItem('theme');
+	if (theme !== null) {
+		if (theme === 'dark') {
+			elements.themeCheckbox.checked = true;
+		}
+		else elements.themeCheckbox.checked = false;
+		document.body.className = theme;
+	}
+}
+
 
 window.onload = async () => {
+	themeController();
+
 	//Add animation to numbers
 	const countUpElements = document.querySelectorAll('.counter');
 	countUpElements.forEach((el) => {
